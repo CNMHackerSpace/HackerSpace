e3218d6b3cc39fe06d5271255438c89d7e1aa58a
Rob Garner <rgarner011235@gmail.com>
2023-04-11 17:06:06 -0600

    Added file upload

---
+++ Server/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] ImageFile file)
+        {
+            var buf = Convert.FromBase64String(file.base64data);
+            string path = env.ContentRootPath + System.IO.Path.DirectorySeparatorChar+"images" + System.IO.Path.DirectorySeparatorChar + Guid.NewGuid().ToString("N") + "-" + file.fileName;
+            await System.IO.File.WriteAllBytesAsync(path, buf);
+        }
+    }
+}
