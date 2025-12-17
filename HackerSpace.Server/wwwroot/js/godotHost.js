1176ced4fe50592360ee71bd4e5e7cee9e7b5e12
Robert Garner <rgarner011235@gmail.com>
2025-12-17 00:16:01 -0700

    Fixed run issues

---
+++ HackerSpace.Server/wwwroot/js/godotHost.js
@@ -0,0 +1,23 @@
+﻿window.godotHost = window.godotHost || {
+    enterFullscreen: function (iframe) {
+        const el = iframe instanceof HTMLElement ? iframe : document.querySelector(".godot-frame");
+        if (!el) return;
+
+        const fn = el.requestFullscreen || el.webkitRequestFullscreen || el.msRequestFullscreen;
+        if (fn) fn.call(el);
+    },
+
+    stop: function (iframe) {
+        // Exit fullscreen if the iframe (or anything) is currently fullscreen
+        try {
+            if (document.fullscreenElement) document.exitFullscreen();
+            else if (document.webkitFullscreenElement) document.webkitExitFullscreen();
+        } catch { }
+
+        // Hard-stop the iframe content so it doesn't keep fetching / painting
+        try {
+            const el = iframe instanceof HTMLElement ? iframe : document.querySelector(".godot-frame");
+            if (el) el.src = "about:blank";
+        } catch { }
+    }
+};
