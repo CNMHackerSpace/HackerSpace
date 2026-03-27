983dd5f92dfb06f1546ee54ffd1852d7b79840f3
Robert Garner <rgarner011235@gmail.com>
2026-03-27 11:04:35 -0600

    add image to user view

---
+++ Server/Data/Migrations/20260327154852_AddBadgeImages.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2025. All rights reserved.
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBadgeImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Badges",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Badges");
+        }
+    }
+}
